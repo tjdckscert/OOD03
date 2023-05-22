dd82b6a8d352ded537eb96e266a2033424aa73e9
tjdckscert <tjdcks1051@naver.com>
2023-05-22 18:59:55 +0900

    Add Repositorys(#3)

---
+++ src/main/java/deu/cse/spring_webmail/Repository/InboxRepository.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package deu.cse.spring_webmail.Repository;
+
+import deu.cse.spring_webmail.Entity.Inbox;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Seongchan
+ */
+@Repository
+public interface InboxRepository extends JpaRepository<Inbox, Integer> {
+//    @Query("select c from Category c where useUserID = :useriextends JpaRepository<Inbox, Integer>d")
+//    public Optional<List<Inbox>> getMails(@Param("userid")String userid);
+    @Query("SELECT i FROM Inbox i WHERE i.messageBody LIKE %:title%")
+    public List<Inbox> searchByMessageBodyLike(@Param("title")String messageBody);
+    
+    public List<Inbox> findByRecipientsOrderByLastUpdated(String recipients);
+    
+//    public Page<Inbox> findByRecipients(String recipients, Pageable pageable);
+    
+    public List<Inbox> findBySenderOrderByLastUpdated(String sender);
+    
+    public List<Inbox> findBySenderAndLastUpdated(String sender, String lastUpdated);
+}
---
+++ src/main/java/deu/cse/spring_webmail/Repository/IsReadRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package deu.cse.spring_webmail.Repository;
+
+import deu.cse.spring_webmail.Entity.IsRead;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author Seongchan
+ */
+public interface IsReadRepository extends JpaRepository<IsRead, Integer>{
+    
+}
